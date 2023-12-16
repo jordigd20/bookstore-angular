c7a7651a11f5002879fa598bb765f1721019dd49
jordigd20 <jordigd8@gmail.com>
2023-12-16 14:14:26 +0100

    fix: environment variables compatible with vercel

---
+++ src/env.d.ts
@@ -0,0 +1,13 @@
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+interface ImportMetaEnv {
+  /**
+   * Built-in environment variable.
+   * @see Docs https://github.com/chihab/ngx-env#ng_app_env.
+   */
+  readonly NG_APP_ENV: string;
+  readonly NG_APP_API_URL: string;
+  [key: string]: any;
+}
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  API_URL: import.meta.env.NG_APP_API_URL,
+};
